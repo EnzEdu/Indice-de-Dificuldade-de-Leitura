2471cf5ae61afcc52d8604da7d4ff9cf0d21196f
EnzEdu <enzinhovlogs@outlook.com>
2023-01-14 23:08:02 -0300

    Refatoração 4 e Indice de Dificuldade

--- exercicio.sh
+++ exercicio.sh
@@ -23,14 +23,17 @@ VALORESPERL=$(perl ./exercicio.pl $PATHARQUIVO)
 NUM_FRASES=$(echo $VALORESPERL | cut -d ':' -f 1)
 NUM_PALAVRAS=$(echo $VALORESPERL | cut -d ':' -f 2)
 NUM_P_DIF=$(echo $VALORESPERL | cut -d ':' -f 3)
-echo "$NUM_FRASES $NUM_PALAVRAS $NUM_P_DIF"
 
-# Media de palavras por frase (num inteiro)
-MP=$(echo "scale=2; $NUM_PALAVRAS/$NUM_FRASES" | bc)
-echo "$MP"
+
+# Media de palavras por frase
+MP=$(echo $NUM_PALAVRAS $NUM_FRASES | awk '{print $1 / $2}')
 
 # Proporcao de palavras dificeis no texto
-PROP_P_DIF=$(echo "scale=2; $NUM_P_DIF/$NUM_PALAVRAS" | bc)
-echo "$PROP_P_DIF"
+PROP_P_DIF=$(echo $NUM_P_DIF $NUM_PALAVRAS | awk '{print $1 / $2}')
+
+# Indice de dificuldade de leitura do texto
+INDICE=$(echo $MP $PROP_P_DIF | awk '{print int(0.4 * ($1 + $2) + 0.5)}')
 
 
+# Resultado do script
+echo $INDICE
